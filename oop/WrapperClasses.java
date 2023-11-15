7bd53f728bf1a78c7d86b057fc9a81d4418fe219
Maelton-SI <mls54@aluno.ifal.edu.br>
2023-11-15 14:52:37 -0300

    wrapper classes added

---
+++ oop/WrapperClasses.java
@@ -0,0 +1,19 @@
+public class WrapperClasses {
+    
+    Byte minhaVariavelByte;
+
+    Short minhaVariavelShort;
+
+    Integer minhaVariavelInt;
+
+    Long minhaVariavelLong = 10L;
+
+    Float minhaVariavelFloat = 5.6f;
+
+    Double minhaVariavelDouble = 7.8;
+
+    Character minhaVariavelChar = 'v';
+
+    Boolean minhaVariavelBoolean;
+}
+
